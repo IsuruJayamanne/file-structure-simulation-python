8e9007d80931f8466ced84314f9907451a5d2096
IsuruJayamanne <48113477+IsuruJayamanne@users.noreply.github.com>
2024-03-23 00:04:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from services.gui import display_folder_structure
 from services.data import get_folder_structure
 
 def main():
-    folder_structure = get_folder_structure()
+    # -----------------------------------
+    # input JSON file name
+    filename='sample_file_structure.json'
+    # -----------------------------------
+
+    # load the data from file
+    folder_structure = get_folder_structure(filename)
+
+    # main code
     display_folder_structure(folder_structure)
 
 if __name__ == "__main__":
